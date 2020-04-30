a024d7d69e3df481dfadd46960397a22f1960772
Texmeijin <i8858garden@gmail.com>
2020-04-30 23:02:48 +0900

    draftjsの導入

--- components/organism/MarkdownEditor.tsx
+++ components/organism/MarkdownEditor.tsx
@@ -1,15 +1,43 @@
-import dynamic from "next/dynamic";
+import React from "react";
 
-const MdEditor = dynamic(() => import("react-markdown-editor-lite"), {
-  ssr: false,
-});
+import { DraftailEditor, BLOCK_TYPE, INLINE_STYLE } from "draftail";
+
+const initial = "";
+const onSave = (content) => {
+  console.log("saving", content);
+};
 
 export default function MarkdownEditor(props) {
   return (
-    <MdEditor
-      config={{ view: { menu: true, md: true, html: false } }}
-      value={props.markdownText}
-      onChange={props.onChange}
+    <DraftailEditor
+      rawContentState={initial || null}
+      onSave={onSave}
+      enableHorizontalRule
+      enableLineBreak
+      showUndoControl
+      autoComplete
+      showRedoControl
+      stripPastedStyles={false}
+      maxListNesting={6}
+      spellCheck
+      blockTypes={[
+        { type: BLOCK_TYPE.HEADER_ONE },
+        { type: BLOCK_TYPE.HEADER_TWO },
+        { type: BLOCK_TYPE.HEADER_THREE },
+        { type: BLOCK_TYPE.HEADER_FOUR },
+        { type: BLOCK_TYPE.ORDERED_LIST_ITEM },
+        { type: BLOCK_TYPE.UNORDERED_LIST_ITEM },
+        { type: BLOCK_TYPE.CODE },
+        { type: BLOCK_TYPE.BLOCKQUOTE },
+      ]}
+      inlineStyles={[
+        { type: INLINE_STYLE.BOLD },
+        { type: INLINE_STYLE.ITALIC },
+        { type: INLINE_STYLE.CODE },
+        { type: INLINE_STYLE.MARK },
+        { type: INLINE_STYLE.QUOTATION },
+        { type: INLINE_STYLE.STRIKETHROUGH },
+      ]}
     />
   );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import { AppProps } from "next/app";
-import "react-markdown-editor-lite/lib/index.css";
+import "../styles/base/reset.scss";
+
+import "draft-js/dist/Draft.css";
+import "draftail/dist/draftail.css";
 import "../styles/markdownEditor/override.css"
-import "../styles/base/reset.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
